319dcaa182f9c11a78cf5c648cd2519129432d8e
Katya <katya.mikhaylova.98@list.ru>
2024-02-13 17:20:38 +0100

    add play btn with animation

--- src/components/LandindComponent/LandindComponent.tsx
+++ src/components/LandindComponent/LandindComponent.tsx
@@ -1,6 +1,4 @@
 import styled, { keyframes } from "styled-components";
-import { TriangleUpIcon } from "@chakra-ui/icons";
-import { IconButton } from "@chakra-ui/react";
 import { LText, MText, XlText } from "components/shared/Text";
 import { DescriptionContainer } from "components/DescriptionContainer/DescriptionContainer";
 import { CarouselContainer } from "components/CarouselContainer/CarouselContainer";
@@ -13,7 +11,7 @@ import {
 } from "components/shared/TextContainer";
 import poster1 from "../../images/poster1.png";
 import poster2 from "../../images/poster2.png";
-import backgroundImg from "../../images/playContainerImg/background.jpeg";
+import { PlayBtnContainer } from "components/PlayBtnContainer/PlayBtnContainer";
 
 const Header = styled.h1`
   position: absolute;
@@ -58,49 +56,6 @@ const MainContainer = styled.div`
   gap: 80px;
 `;
 
-const PlayContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 300px;
-  background-color: black;
-  margin: 20px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const InsidePlayContainer = styled.div`
-  width: 90vw;
-  height: 100%;
-  background-image: url(${backgroundImg});
-  background-size: cover;
-  background-position: center;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 20%;
-`;
-
-const Line = styled.div`
-  position: absolute;
-  left: 0;
-  top: 50%;
-  width: 100%;
-  height: 5px;
-  background-color: #fe5401;
-`;
-
-const Button = styled.button`
-  width: 120px;
-  height: 120px;
-  border-radius: 100%;
-  background-color: #000000;
-  color: #fe5401;
-  font-weight: 800;
-  z-index: 5;
-`;
-
 export const LandingComponent = () => {
   return (
     <div>
@@ -136,20 +91,7 @@ export const LandingComponent = () => {
         <FlipCardsContainer />
         <ServiceContainer />
         <ImageContainer src={poster2} alt="poster2" />
-
-        <PlayContainer>
-          <InsidePlayContainer>
-            <Button type="button">
-              <IconButton
-                aria-label="Play"
-                icon={<TriangleUpIcon transform="rotate(90deg)" />}
-                onClick={() => console.log("Play")}
-                boxSize="20px"
-              />
-            </Button>
-          </InsidePlayContainer>
-          <Line />
-        </PlayContainer>
+        <PlayBtnContainer />
       </MainContainer>
     </div>
   );
